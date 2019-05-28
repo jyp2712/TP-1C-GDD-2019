92acd3e14470fa53ea9a1419663062366c43f21c
Nicolás Goreico <ngoreico@gmail.com>
2019-05-27 19:43:45 -0700

    Conexion con DB. El login ya checkea con password SHA256.

---
+++ DB/DBConnection.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Forms;
+using System.Data;
+
+namespace FrbaCrucero.DB
+{
+    class DBConnection
+    {
+
+        private static DBConnection dbConnection;
+
+        public static DBConnection getInstance()
+        {
+            if (dbConnection == null)
+            {
+                DBConnection db = new DBConnection();
+                db.connect();
+                dbConnection = db;
+            }
+
+            return dbConnection;           
+        }
+
+        string dbHost = ConfigurationManager.AppSettings["dbHost"];
+        string dbName = ConfigurationManager.AppSettings["dbName"];
+        string username = ConfigurationManager.AppSettings["username"];
+        string password = ConfigurationManager.AppSettings["password"];
+
+        private SqlConnection connection;
+
+        private void connect()
+        {
+            SqlConnectionStringBuilder stringConnectionBuilder = new SqlConnectionStringBuilder();
+            stringConnectionBuilder.DataSource = dbHost;
+            stringConnectionBuilder.InitialCatalog = dbName;
+            stringConnectionBuilder.UserID = username;
+            stringConnectionBuilder.Password = password;
+
+            try
+            {
+                this.connection = new SqlConnection(stringConnectionBuilder.ConnectionString);
+            }
+            catch (Exception e) //TODO ver si hay que logear...
+            {
+                DialogResult result;
+                result = MessageBox.Show("Error al conectar con la base de datos. La aplicación se cerrará.", "ERROR !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        public DataSet executeQuery(String query)
+        {
+            try
+            {
+                connection.Open();
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                {
+                    DataSet result = new DataSet();
+                    adapter.Fill(result);
+                    return result;
+                }
+            }
+            catch (SqlException e)
+            {
+                DialogResult result;
+                result = MessageBox.Show("Error al conectar con la base de datos. La aplicación se cerrará." + e.Message, "ERROR !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+            finally {
+                if (connection.State == ConnectionState.Open) connection.Close();
+            }
+        }
+    }
+
+}
--- Login/LoginForm.cs
+++ Login/LoginForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FrbaCrucero.DB;
+using System.Security.Cryptography;  
 
 namespace FrbaCrucero.Login
 {
@@ -30,6 +32,7 @@ namespace FrbaCrucero.Login
         {
             if (this.checkUserAndPassword())
             {
+                MessageBox.Show("VAAAMOO");
                 //TODO abrir form con cosas de administrador
                 //TODO setear en 0 la cantidad de reintentos de login.
             }
@@ -54,7 +57,33 @@ namespace FrbaCrucero.Login
 
         private Boolean checkUserAndPassword() {
             //TODO ir a la DB.
-            return false; //TODO
+            DBConnection dbConnection = DBConnection.getInstance();
+            //string query = "SELECT * FROM [GD1C2019].[dbo].[User] WHERE user_usuario='admin' AND user_contrasenia='E6B87050BFCB8143FCB8DB0170A4DC9ED00D904DDD3E2A4AD1B1E8DC0FDC9BE7'";
+            string sha256Password =  "\'" + toSHA256(this.txtPassword.Text) + "\'";
+            string username = "\'" + this.txtUser.Text + "\'";
+            string query = "SELECT * FROM [GD1C2019].[dbo].[User] WHERE user_usuario=" + username + " AND user_contrasenia=" + sha256Password;
+            DataSet ds = dbConnection.executeQuery(query);
+            int r = ds.Tables[0].Rows.Count;
+            return ds.Tables[0].Rows.Count != 0;
         }
+
+        private string toSHA256(String rawData) 
+        {  
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())  
+            {  
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));  
+  
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();  
+                for (int i = 0; i < bytes.Length; i++)  
+                {  
+                    builder.Append(bytes[i].ToString("x2"));  
+                }  
+                return builder.ToString();  
+            }  
+        }  
+       
     }
 }
